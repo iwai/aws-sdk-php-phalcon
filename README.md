f664fb902d4548d0128a605f06d095af41d355fb
Yuji Iwai <iwai@sonicmoov.com>
2013-08-12 17:02:54 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-aws-sdk-php-phalcon
+AWS Service Provider for Phalcon
 ===================
 
 A simple Phalcon service provider for including the AWS SDK for PHP.
+
+Installation
+============
+
+```json
+{
+    "require": {
+        "iwai/aws-sdk-php-phalcon": "*"
+    }
+}
+```
+
+Usage
+=====
+
+```php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Aws\Phalcon\AwsServiceProvider;
+use Phalcon\Mvc\Application;
+
+$app = new Application();
+
+$eventsManager = new Phalcon\Events\Manager();
+$app->setEventsManager($eventsManager);
+
+$eventsManager->attach("application:boot", new AwsServiceProvider(array(
+    'key'    => 'your-aws-access-key-id',
+    'secret' => 'your-aws-secret-access-key',
+    'region' => 'us-east-1',
+)));
+
+echo $app->handle()->getContent();
+
+```
---
+++ src/Aws/Phalcon/AwsServiceProvider.php
@@ -0,0 +1,66 @@
+<?php
+ /**
+  * AwsServiceProvider.php
+  *
+  * @copyright   Copyright (c) 2013 Yuji Iwai.
+  * @package     
+  * @subpackage  
+  * @version     $Id$
+  */
+
+namespace Aws\Phalcon;
+
+use Phalcon\Mvc\Application;
+use Aws\Common\Aws;
+use Aws\Common\Client\UserAgentListener;
+use Guzzle\Common\Event;
+use Guzzle\Service\Client;
+
+class AwsServiceProvider {
+
+    const DEFAULT_REGION = 'us-east-1';
+
+    private $options;
+
+    /**
+     * @param array $options
+     */
+    function __construct($options = null)
+    {
+        if (!$options)
+            throw new Application\Exception('No configuration given');
+
+        if (!isset($options['key']))
+            throw new Application\Exception('Not key configuration given');
+        if (!isset($options['secret']))
+            throw new Application\Exception('Not key configuration given');
+        if (!isset($options['region']))
+            $options['region'] = self::DEFAULT_REGION;
+
+        $this->options = $options;
+    }
+
+    /**
+     * @param Application $app
+     */
+    public function boot(Application $app)
+    {
+        $options = $this->options;
+
+        $app->getDI()->setShared('aws', function ($options) {
+            $aws = Aws::factory($options);
+
+            $aws->getEventDispatcher()->addListener('service_builder.create_client', function (Event $event) {
+                $clientConfig = $event['client']->getConfig();
+
+                $commandParams = $clientConfig->get(Client::COMMAND_PARAMS) ?: array();
+
+                $clientConfig->set(Client::COMMAND_PARAMS, array_merge_recursive($commandParams, array(
+                    UserAgentListener::OPTION => 'Phalcon/' . Application::VERSION
+                )));
+            });
+
+            return $aws;
+        });
+    }
+}
